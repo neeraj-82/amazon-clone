ef8764a42b433409258a78b653c4a7cb55c7d0df
Amit singh <neerajgusain317@gmail.com>
2021-01-01 11:45:20 +0530

    change the name of variables

--- src/Home.js
+++ src/Home.js
@@ -1,78 +1,86 @@
 import React, { useEffect, useState } from 'react'
 import './HOme.css';
 import Product from './Product';
-import Data, { pro } from './Data.js';
+import Productlistfirst, { productlistsecond } from './Data.js';
 import { useStateValue } from './StateProvider';
 
 function Home() {
     const [{ searchlist }, dispatch] = useStateValue();
-    const [search_pro, set_search_pro] = useState([]);
-    // const [search_pro_two, set_search_pro_two] = useState([]);
-    console.log(searchlist);
+    const [search_productfirst, set_search_productfirst] = useState([]);
+    const [search_producttwo, set_search_producttwo] = useState([]);
+    // console.log(searchlist);
 
+
+
+    // whenever the searchlist is change it found the product in product lists
     useEffect(() => {
 
-        const Search_item = Data.filter((product) => {
+        const Search_item = Productlistfirst.filter((product) => {
             return ((product.title.toUpperCase())).includes(searchlist.toUpperCase());
         })
-        set_search_pro(Search_item);
+        set_search_productfirst(Search_item);
 
-        // const Search_item_two = pro.filter((product) => {
-        //     return ((product.title.toUpperCase())).includes(searchlist.toUpperCase());
-        // })
-        // set_search_pro_two(Search_item_two);
+        const Search_item_two = productlistsecond.filter((product) => {
+            return ((product.title.toUpperCase())).includes(searchlist.toUpperCase());
+        })
+        set_search_producttwo(Search_item_two);
 
     }, [searchlist]);
 
 
-    // console.log(search_pro);
+
     return (
         <>
-            
-            
-         
-                <div className="home">
 
-                    {!searchlist? <img
-                        className="home__image"
-                        src='https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._SY1200_FMJPG_.jpg'
-                        alt="err"
-                    />:null}
-                    <div className="home__rowOne">
-                        {
-                           searchlist? 
-                            search_pro.map((data)=>{
-                            return <Product key={data.id} id={data.id} title={data.title} image={data.image} price={data.price} rating={data.rating} />
-                              
-                           }):
-                                Data.map((data) => {
-                                    return <Product key={data.id} id={data.id} title={data.title} image={data.image} price={data.price} rating={data.rating} />
-                                })
-                        }
-                    </div>
-                 { <img
-                        className="home__add"
-                        src="https://rukminim1.flixcart.com/flap/1800/1800/image/8dc8ec2644e9a2ca.jpg?q=50"
-                        alt="err"
-                    />}
-                    <div className="home__rowOne">
-                        {
-                            
-                                pro.map((data) => {
-                                    return <Product key={data.id} id={data.id} title={data.title} count={data.count} image={data.image} price={data.price} rating={data.rating} />
-                                })
-                        }
-                    </div>
 
+
+            <div className="home">
+
+                {!searchlist ? <img
+                    className="home__image"
+                    src='https://m.media-amazon.com/images/G/31/AmazonVideo/2019/1102620_MLP_1440x675_apv189_V3._SY1200_FMJPG_.jpg'
+                    alt="err"
+                /> : null}
+                <div className="home__rowOne">
+                    {
+                        searchlist ?
+                            search_productfirst.map((data) => {
+                                return <Product key={data.id} id={data.id} title={data.title} image={data.image} price={data.price} rating={data.rating} />
+
+                            }) :
+                            Productlistfirst.map((data) => {
+                                return <Product key={data.id} id={data.id} title={data.title} image={data.image} price={data.price} rating={data.rating} />
+                            })
+                    }
+                </div>
+                {searchlist ? null : <img
+                    className="home__add"
+                    src="https://rukminim1.flixcart.com/flap/1800/1800/image/8dc8ec2644e9a2ca.jpg?q=50"
+                    alt="err"
+                />}
+                <div className="home__rowOne">
+                    {
+                        searchlist ?
+                            search_producttwo.map((data) => {
+                                return <Product key={data.id} id={data.id} title={data.title} image={data.image} price={data.price} rating={data.rating} />
+
+                            }) :
+                            productlistsecond.map((data) => {
+                                return <Product key={data.id} id={data.id} title={data.title} count={data.count} image={data.image} price={data.price} rating={data.rating} />
+                            })
+                    }
                 </div>
 
-                {search_pro.length?null:
-                 <div className='home__notFound'>
-                   
-                     <h1>:not found</h1>
-                 </div> 
-                }
-            
+            </div>
+
+            {/* if product not found */}
+            {search_productfirst.length || search_producttwo.length ? null :
+                <div className='home__notFound'>
+
+                    <h1>:not found</h1>
+                </div>
+            }
+
 
 
         </>
